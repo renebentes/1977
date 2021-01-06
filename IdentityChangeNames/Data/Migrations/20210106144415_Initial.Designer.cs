0fc75c62e8592f3ceeed5e5df8c1cc3ae074c4e2
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-01-06 11:45:52 -0300

    Cria migration inicial

--- IdentityChangeNames/Data/Migrations/20210106144415_Initial.Designer.cs
+++ IdentityChangeNames/Data/Migrations/20210106144415_Initial.Designer.cs
@@ -4,14 +4,16 @@ using IdentityChangeNames.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdentityChangeNames.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210106144415_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
