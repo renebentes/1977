c83b6ef3bb4c137bdba351ca288c2a79c9749a32
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-03-10 19:54:13 -0300

    Desabilita External Providers sem uso

--- ExternalProviders/Startup.cs
+++ ExternalProviders/Startup.cs
@@ -1,4 +1,3 @@
-using System.Security.Claims;
 using ExternalProviders.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
@@ -8,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Claims;
 
 namespace ExternalProviders
 {
@@ -63,48 +63,48 @@ namespace ExternalProviders
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            services.AddAuthentication()
-                .AddFacebook(facebookOptions =>
-                {
-                    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
-                    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            services.AddAuthentication();
+            //     .AddFacebook(facebookOptions =>
+            //     {
+            //         facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+            //         facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
 
-                    // Obter dados por escopo - apenas após validação do app pelo Facebook
-                    facebookOptions.Scope.Add("user_birthday");
+            //         // Obter dados por escopo - apenas após validação do app pelo Facebook
+            //         facebookOptions.Scope.Add("user_birthday");
 
-                    // Obter locale usado pelo usuário
-                    facebookOptions.ClaimActions.MapJsonKey(ClaimTypes.Locality, "locale");
+            //         // Obter locale usado pelo usuário
+            //         facebookOptions.ClaimActions.MapJsonKey(ClaimTypes.Locality, "locale");
 
-                    facebookOptions.SaveTokens = true;
-                })
-                .AddGoogle(googleOptions =>
-                {
-                    googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
-                    googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-                    googleOptions.SaveTokens = true;
-                })
-                .AddTwitter(twitterOptions =>
-                {
-                    twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
-                    twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
-                    twitterOptions.SaveTokens = true;
+            //         facebookOptions.SaveTokens = true;
+            //     })
+            //     .AddGoogle(googleOptions =>
+            //     {
+            //         googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+            //         googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            //         googleOptions.SaveTokens = true;
+            //     });
+            // .AddTwitter(twitterOptions =>
+            //     {
+            //         twitterOptions.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+            //         twitterOptions.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+            //         twitterOptions.SaveTokens = true;
 
-                    twitterOptions.RetrieveUserDetails = true;
+            //         twitterOptions.RetrieveUserDetails = true;
 
-                    twitterOptions.ClaimActions.MapJsonKey(ClaimTypes.Email, "email", ClaimValueTypes.Email);
-                })
-                .AddMicrosoftAccount(maOptions =>
-                {
-                    maOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
-                    maOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
-                    maOptions.SaveTokens = true;
-                })
-                .AddLinkedIn(linkedInOptions =>
-                {
-                    linkedInOptions.ClientId = Configuration["Authentication:LinkedIn:ClientId"];
-                    linkedInOptions.ClientSecret = Configuration["Authentication:LinkedIn:ClientSecret"];
-                    linkedInOptions.SaveTokens = true;
-                });
+            //         twitterOptions.ClaimActions.MapJsonKey(ClaimTypes.Email, "email", ClaimValueTypes.Email);
+            //     })
+            //     .AddMicrosoftAccount(maOptions =>
+            //     {
+            //         maOptions.ClientId = Configuration["Authentication:Microsoft:ClientId"];
+            //         maOptions.ClientSecret = Configuration["Authentication:Microsoft:ClientSecret"];
+            //         maOptions.SaveTokens = true;
+            //     })
+            //     .AddLinkedIn(linkedInOptions =>
+            //     {
+            //         linkedInOptions.ClientId = Configuration["Authentication:LinkedIn:ClientId"];
+            //         linkedInOptions.ClientSecret = Configuration["Authentication:LinkedIn:ClientSecret"];
+            //         linkedInOptions.SaveTokens = true;
+            //     });
 
             services.AddControllersWithViews();
         }
