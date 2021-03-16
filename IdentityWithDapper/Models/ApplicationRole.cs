ea2170ab2320fcfe8e7291fc61e569b3e8ed8fd1
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-03-15 21:07:00 -0300

    Define modelo de dados

---
+++ IdentityWithDapper/Models/ApplicationRole.cs
@@ -0,0 +1,11 @@
+namespace IdentityWithDapper.Models
+{
+    public class ApplicationRole
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string NormalizedName { get; set; }
+    }
+}
