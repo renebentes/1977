cc5e54c75bcd9876f126e58c84fe69dd238851a8
Rene Bentes Pinto <renebentes@yahoo.com.br>
2021-03-29 21:03:07 -0300

    Altera Startup para usar modelos definidos

--- IdentityWithDapper/Startup.cs
+++ IdentityWithDapper/Startup.cs
@@ -1,5 +1,8 @@
+using IdentityWithDapper.Data;
+using IdentityWithDapper.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -48,6 +51,13 @@ namespace IdentityWithDapper
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<IUserStore<ApplicationUser>, UserStore>();
+            services.AddTransient<IRoleStore<ApplicationRole>, RoleStore>();
+
             services.AddControllersWithViews();
         }
     }
